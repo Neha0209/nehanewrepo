c42919f6e989766b3f63fa46cdf3e491865b3b83
Neha0209 <42274446+Neha0209@users.noreply.github.com>
2018-08-20 22:53:52 +0530

    Add files via upload

---
+++ git-commands.java
@@ -0,0 +1,23 @@
+git config
+Sets configuration values for your user name, email, gpg key, preferred diff algorithm, file formats and more.
+
+git init
+Initializes a git repository – creates the initial .git directory in a new or in an existing project. 
+
+git add
+Adds files changes in your working directory to your index. 
+
+git commit
+Takes all of the changes written in the index, creates a new commit object pointing to it and sets the branch to point to that new commit. 
+
+git status
+Shows you the status of files in the index versus the working directory. It will list out files that are untracked (only in your working directory), modified (tracked but not yet updated in your index), and staged (added to your index and ready for committing).
+
+git pull
+Fetches the files from the remote repository and merges it with your local one. 
+
+git push
+Pushes all the modified local objects to the remote repository and advances its branches. 
+
+git remote
+Shows all the remote versions of your repository. 
